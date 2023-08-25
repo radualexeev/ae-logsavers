1231b5dc354a3e5834f07815fbe5db7b37b9574c
Radu Alexeev <1radualexeev1@gmail.com>
2023-08-25 16:32:30 +0300

    change font family to serif

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'montserrat': ['Montserrat', 'sans-serif']
+        'montserrat': ['Montserrat', 'serif']
       }
     },
   },
