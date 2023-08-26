968c642729dce2e1d00c273bf160a611dc6709d5
Radu Alexeev <1radualexeev1@gmail.com>
2023-08-26 16:26:16 +0300

    clear up

--- client/src/components/FirstPage.jsx
+++ client/src/components/FirstPage.jsx
@@ -1,28 +1,9 @@
 export default function FirstPage() {
     return (
     <div className=" text-white flex flex-col items-center justify-start relative w-full h-screen z-10">
-      {/* ------------------------------------------------ */}
-      <div className="w-full h-24 flex items-center ">
-        <ul className="w-full flex flex-row justify-between items-center text-sm font-bold tracking-widest uppercase">
-          <li className="w-1/3 flex justify-start gap-10 pl-8">
-            <a className="" href="/">Services</a>
-            <a href="/">Works</a>
-            <a href="/">About</a>
-          </li>
-          <li className="w-1/3 flex justify-center text-3xl">AE LOGSAVERS</li>
-          <li className="w-1/3 flex justify-end gap-10 pr-8">
-            <a href="/">Team</a>
-            <a href="/">Achievements</a>
-            <a href="/">Contacts</a>
-            <a href="/">Search</a>
-          </li>
-        </ul>
-      </div>
-
-      {/* ------------------------------------------------ */}
       
       <div className="w-full h-5/6 flex flex-col items-center justify-center ">
-        <h1 className="w-3/5 p-10 uppercase text-center leading-snug text-7xl font-thin">a new generation of Logistics Transportation</h1>
+        <h1 className="w-3/5 p-10 uppercase text-center leading-snug text-7xl font-thin">a new generation of media agency</h1>
         <p className="w-3/5 p-5 text-center text-xl font-thin">Our experience is hidden under mountains of hard work</p>
         <button className="my-16 border-solid border-2 uppercase tracking-wider rounded-2xl py-2 px-5">Find Your Quote</button>
       </div>
